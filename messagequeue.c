7aae213964ab5f7fae61852336c9640388844841
Peter O'Donnell <pdspeter@gmail.com>
2012-03-22 13:51:50 +0000

    second commit

---
+++ messagequeue.c
@@ -0,0 +1,133 @@
+#include "messagequeue.h"
+#include "ao.h"
+#include <Stdio.h>
+#include <stdlib.h>
+
+#define MESSAGE_QUEUE_LIST_SIZE 10000
+
+struct task_t{
+//	void (*tsk)(void*);
+	void *data;
+};
+
+/* represents node in a list (messages) */
+struct message_t{
+	
+	 struct message_t *N;
+	 struct message_t *P;
+	unsigned int num_of_msgs;
+	task msgs;
+	char id;
+};
+
+struct message_list_t{
+	message start;
+	message end;
+	unsigned int msg_count;
+
+};
+
+struct messageQueue{
+	mlist msgs;	
+	list in_use;
+
+	AO_MUTEX_T queue_mutex;
+
+};
+
+struct in_use_node{
+	char *id;
+	struct in_use_node *next;
+	struct in_use_node *prev;
+
+};
+
+struct in_use_list{
+	node head;
+	node tail;
+	int node_count;
+};
+
+/*
+ * IN_USE_LIST SECTION
+ */
+
+/* Initialise an empty list */
+list init_list(void){
+	list l;
+	l = (list)malloc(sizeof(struct in_use_list));
+	if(l== NULL){
+		return l;
+	}
+	l->head=NULL;
+	l->tail=NULL;
+	l->node_count = 0;
+	return l;
+}
+/* create a node for the in use list */
+node Create_Node( char *id){
+
+	node n;
+	
+	n = (node) malloc(sizeof(struct in_use_node));
+	if (n == NULL){
+		return n;
+	}
+
+	n->id = id;
+	n->prev = NULL;
+	n->next = NULL;
+	
+	return n;
+}
+
+/* Add am id to the in use list note: this is not appended */
+node add_id(list l, node n){
+	/* check if parameters are null*/
+	if(l ==NULL || n == NULL){
+		return NULL;
+	}
+
+	if(l->head == NULL){
+		l->head = n;
+		l->tail = n;
+	}
+
+	else{/* non - empty list */
+		l->tail->next = n;
+	 	n->prev = l->tail;
+		l->tail = n;	
+	
+	}
+
+	l->node_count;	
+	return n;
+}
+
+/* search for an id in the list */
+node find_id(list l, char *id){
+	/* local node used as iterator node */
+	node iterator;
+
+	/* check if list is null or empty */
+	if(l == NULL || l->head == NULL){
+		return NULL;
+	}
+	/* loop to check if is present */
+	for(iterator = l->head; iterator != NULL; iterator = iterator->next){
+		if(iterator->id == id){
+			return iterator;
+		}
+
+
+	}/* end of search loop */
+
+	/* id not found */
+	return NULL;
+}
+
+
+
+
+
+
---
+++ messagequeue.h
@@ -0,0 +1,59 @@
+#ifndef MESSAGEQUEUE_H
+#define MESSAGEQUEUE_H
+
+
+typedef struct task_t *task;
+typedef struct message_t *message;
+typedef struct message_list_t *mlist;
+typedef struct messageQueue *queue;
+typedef struct in_use_list *list;
+typedef struct in_use_node *node;
+
+#define RAISE_ERROR(...) fprintf(stderr,"line %d - ", __LINE__); fprintf(stderr, __VA_ARGS__); fprintf (stderr,"\n")
+
+
+/*
+ * 	Function:	init_list
+ * 	
+ * 	Parameter:	void
+ *
+ * 	Returns:	list
+ *
+ * 	This functions initialises an empty list
+ */
+list init_list(void);
+
+/*
+ * 	Function:	Create_Node
+ *
+ * 	Parameters:	char id
+ *
+ * 	Returns:	Node
+ *
+ * 	This functions creates a new node for the in_use_list
+ */
+node Create_Node( char *id);
+
+/*
+ * 	Function:	add_id
+ *
+ * 	Parameters:	list l, node n
+ *
+ * 	Returns:	Node
+ *
+ * 	This functions adds a node which contains an object id to the list
+ */
+node add_id(list l, node n);
+
+
+/*
+ * 	Function:	add_id
+ *
+ * 	Parameters:	 list l, char id
+ *
+ * 	Returns:	void
+ *
+ * 	This function searches for an id in the list
+ */
+node find_id(list l, char *id);
+#endif
